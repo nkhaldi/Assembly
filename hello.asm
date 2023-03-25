588fd74b50faeb21d563fa572bffbd6fb3f1a8df
nkhaldi <mnk80898@gmail.com>
2023-03-25 22:00:17 +0300

    modified hello world program

--- hello.asm
+++ hello.asm
@@ -1,15 +1,16 @@
-; Эта программа выводит "Hello world!"
+; Эта программа выводит "Hello, world!"
 
-section     .text
-global      _start 
-_start: 
-    mov     edx,len   
-    mov     ecx,msg   
-    mov     ebx,1   
-    mov     eax,4   
-    int     0x80   
-    mov     eax,1  
-    int     0x80   
-section     .data
-msg     db  'Hello world',0xa  
-len     equ $ - msg
+global _start
+
+section .data
+message: db 'hello, world!', 10
+
+section .text
+_start:
+    mov     rax, 1          ; system call number should be stored in rax
+    mov     rdi, 1          ; argument #1 in rdi: where to write (descriptor)?
+    mov     rsi, message    ; argument #2 in rsi: where does the string start?
+    mov     rdx, 14         ; argument #3 in rdx: how many bytes to write?
+    syscall
+
+; this instruction invokes a system call
